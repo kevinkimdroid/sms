f83fdee78053a949e1760a601cae437ec7be78bb
unknown <kelvinkimutai1@gmail.com>
2023-03-27 20:20:02 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel 9 CRUD Tutorial Example</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Laravel 9 CRUD Example Tutorial</h2>
+                </div>
+                <div class="pull-right mb-2">
+                    <a class="btn btn-success" href="{{ route('companies.create') }}"> Create Company</a>
+                </div>
+            </div>
+        </div>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>S.No</th>
+                    <th>Company Name</th>
+                    <th>Company Email</th>
+                    <th>Company Address</th>
+                    <th width="280px">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($companies as $company)
+                    <tr>
+                        <td>{{ $company->id }}</td>
+                        <td>{{ $company->name }}</td>
+                        <td>{{ $company->email }}</td>
+                        <td>{{ $company->address }}</td>
+                        <td>
+                            <form action="{{ route('companies.destroy',$company->id) }}" method="Post">
+                                <a class="btn btn-primary" href="{{ route('companies.edit',$company->id) }}">Edit</a>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+            </tbody>
+        </table>
+        {!! $companies->links() !!}
+    </div>
+</body>
+</html>
+create.blade.php:
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Add Company Form - Laravel 9 CRUD</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left mb-2">
+                    <h2>Add Company</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('companies.index') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="{{ route('companies.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Company Name:</strong>
+                        <input type="text" name="name" class="form-control" placeholder="Company Name">
+                        @error('name')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Company Email:</strong>
+                        <input type="email" name="email" class="form-control" placeholder="Company Email">
+                        @error('email')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Company Address:</strong>
+                        <input type="text" name="address" class="form-control" placeholder="Company Address">
+                        @error('address')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
